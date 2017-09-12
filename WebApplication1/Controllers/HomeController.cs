81f3654bd8a6a5905bd66a200f8b9e2f68f596b9
Andrei Rinea <andrei@rinea.ro>
2017-09-12 04:38:17 +0300

    final state

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using helloReact.Models;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace helloReact.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel> {
+                new CommentModel { Id = 1, Author = "Edgar", Text = "Y U No Right unit tezts?!" }
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        public IActionResult React()
+        {
+            return View();
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Comments()
+        {
+            return Json(_comments);
+        }
+
+        [Route("comments/new")]
+        [HttpPost]
+        public IActionResult AddComment(CommentModel comment)
+        {
+            comment.Id = _comments.Select(c => c.Id).Max() + 1;
+            _comments.Add(comment);
+            return Content("Success :)");
+        }
+    }
+}
