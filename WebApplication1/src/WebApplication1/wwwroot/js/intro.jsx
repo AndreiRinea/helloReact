b36fb6c9beb25adce64fccf1fded4418af59bdcd
Andrei Rinea <arinea@intralinks.com>
2017-09-12 15:43:39 +0300

    componentization

--- WebApplication1/src/WebApplication1/wwwroot/js/intro.jsx
+++ WebApplication1/src/WebApplication1/wwwroot/js/intro.jsx
@@ -1,12 +1,52 @@
-﻿var CommentBox = React.createClass({
-    render: function() {
+﻿var Comment = React.createClass({
+    render: function () {
         return (
-          <div className="commentBox">
-            Hello, world! I am a CommentBox.
+          <div className="comment">
+            <h4 className="commentAuthor">
+                {this.props.author}
+            </h4>
+              {this.props.children}
           </div>
       );
     }
 });
+
+var CommentList = React.createClass({
+    render: function () {
+        return (
+          <div className="commentList">
+              <Comment author="Jim">Nice.</Comment>
+              <Comment author="Joe">Interesting...</Comment>
+              <Comment author="Jack">Hm.</Comment>
+          </div>
+      );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function () {
+        return (
+          <div className="commentForm">
+              Hello, world! I am a CommentForm.
+          </div>
+      );
+    }
+});
+
+var CommentBox = React.createClass({
+    render: function () {
+        return (
+          <div className="commentBox">
+                <h3>Comments</h3>
+                <CommentList />
+                <hr />
+                <CommentForm />
+          </div>
+      );
+    }
+});
+
+//////////////////////////////////////////////////////
 
 ReactDOM.render(
   <CommentBox />,
